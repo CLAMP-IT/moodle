4dc215abd8cfd92e4f001482104a84a94e4594f6
Matt Elson <melson@mugen.wesleyan.edu>
2012-07-05 11:17:21 -0400

    Wes - Added wessync enrolment plugin.

---
+++ enrol/wessync/cli/enrolsync.php
@@ -0,0 +1,152 @@
+<?php
+/* unfortunately not well thought out - whether something is in helperlib
+   or the enrol object is vaguely random feeling right now; will try and
+   refactor - might make sense for different objects for each data source;
+   i.e. moodlecreate+peoplesoft vs. AD + hash, etc, etc.. */
+
+define('CLI_SCRIPT', true);
+
+require(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+
+// Ensure errors are well explained
+$CFG->debug = DEBUG_NORMAL;
+
+if (!enrol_is_enabled('wessync')) {
+    die;
+}
+
+// Update enrolments -- these handlers should autocreate courses if required
+$enrol = enrol_get_plugin('wessync');
+require(dirname(dirname(__FILE__)).'/helperlib.php');
+$lock = check_lock_file($argv[1]);
+if ($argv[1] == 'peoplesoft_enrol' ) {
+  $results = peoplesoft_enrol($enrol,$lock);
+} else if ($argv[1] == 'ldap_enrol' ) {
+  $results = ldap_enrol($enrol,$lock);
+} else if ($argv[1] == 'fy_enrol') {
+  $results = fy_enrol($enrol,$lock);
+} else {
+  print "Unknown enrol request!";
+}
+release_lock_file($lock,$argv[1]);
+
+function peoplesoft_enrol ($enrol,$lock) {
+  $master_results = array();
+
+  $moodlecreate = get_moodlecreate_db();
+  if (mysqli_connect_errno()) {
+    print mysqli_connect_error();
+    release_lock_file($lock,'peoplesoft_enrol');
+    die;
+  }
+  $ps89prod = get_ps89prod_db();
+  if (!$ps89prod) {
+    print oci_error($ps89prod);
+    release_lock_file($lock,'peoplesoft_enrol');
+    die;
+  }
+  $semester = $enrol->get_current_wes_semester();
+  /* handle the moodlecreate creations */
+  $moodlecreate_courses = get_moodlecreate_courses($moodlecreate,$semester);
+  $email_results = array();
+  foreach ($moodlecreate_courses as $course) {
+    $moodle_course = get_moodle_course($course['idnumber']);
+    if (!$moodle_course) {
+      print "Course " . $course['short_name'] . " does not exist, creating...\n";
+      if ($course['status'] == 'Y' ) {
+	print "Course " . $course['short_name'] . " says it is already created, refusing to continue.\n";
+        continue;
+      } else {
+        $course = get_peoplesoft_course_data($ps89prod,$course);
+        $moodle_course = $enrol->create_moodle_course_from_template($course);
+        if ($moodle_course) {
+  	  /*call back to MoodleCreate database to flag as created */
+  	 # flag_as_created($moodlecreate,$course['id']);
+	  $email_results[] = $course['short_name'] . " newly created.";
+	  $email_results[] = "\tRequested by:" . $course['requested_by'];
+        } else {
+	  print "Could not create course " . $course['short_name'] . " because of the following:\n" . implode('\n',$enrol->ERRORS) . "\n";
+        }
+      }
+    }
+    if (!$moodle_course) {
+      continue;
+    }
+    $auth_students = $enrol->get_members_from_ps89prod($moodle_course,$ps89prod);
+    if ($auth_students === false ) {
+      print "Database errors";
+      continue;
+    }
+    $auth_teachers = $enrol->get_instructors_from_ps89prod($moodle_course,$ps89prod);
+    if ($email_results) {
+      foreach ($auth_teachers as $teacher) {
+        $email_results[] = "\tInstructor: $teacher";
+      }
+    }    
+    if ($auth_teachers === false ) {
+      print "Database errors";
+      continue;
+    }
+    /* role id 5 == student */
+    $result = $enrol->sync_course_membership_by_role($moodle_course,$auth_students,"5");
+    $master_results[] = $result;
+    /* role id 3 == teacher */
+    $result = $enrol->sync_course_membership_by_role($moodle_course,$auth_teachers,"3");
+    $master_results[] = $result;
+  } 
+  if ($email_results) {
+     $body = implode("\n",$email_results);
+     mail("melson@wesleyan.edu","Moodle Course Creation Report",$body,"From: moodle_admins@wesleyan.edu");
+  }
+  return $master_results;
+}
+
+/*alright, now for one offs defined by LDAP groups */
+function ldap_enrol ($enrol,$lock) {
+  global $DB;
+  $ldapauth = get_auth_plugin('ldap');
+  $ldapconnection = $ldapauth->ldap_connect();
+  #format is Moodle ShortName => array of AD groups
+  $one_off_syncs = array( 'CBC-Disc' => array('list_all_faculty','list_librarians','list_admin_fac_priv'),
+                          'Staff-Disc' => array('list_ben_astf'),
+                          'AdHocCommRpts' => array ('list_all_faculty','list_admin_fac_priv'), 
+                          'faculty-chair' => array('voting_faculty'),
+                          'Host-Training' => array('2012'));
+
+
+  foreach ($one_off_syncs as $course => $ldap_groups) {
+    $moodle_course = $DB->get_record('course',array('shortname' => $course));
+    if (!$moodle_course) {
+      /*no such Moodle course */
+      continue;
+    }
+    $authoritative_members = array();
+    foreach ($ldap_groups as $ldap_group) {
+      $group_members = get_from_ad($ldap_group,$ldapconnection);
+      $authoritative_members = array_merge($authoritative_members,$group_members);
+    }
+    $master_results[] = $enrol->sync_course_membership_by_role($moodle_course,$authoritative_members,"5");
+  }
+  return $master_results;
+}
+
+/*alright, now for first year categories */
+function fy_enrol ($enrol,$lock) {
+  $master_results = array();
+  $semester = $enrol->get_current_wes_semester();
+  $ps89prod = get_ps89prod_db();
+  if (!$ps89prod) {
+    print oci_error($ps89prod);
+    release_lock_file($lock,'fy_enrol');
+    die;
+  }
+  $fy_courses = wes_get_first_year_courses($semester);
+  $fy_students = wes_get_first_year_students($ps89prod,$semester);
+  
+  foreach ($fy_courses as $moodle_course) {
+     $result = $enrol->sync_course_membership_by_role($moodle_course,$fy_students,"5");
+     $master_results[] = $result;
+  }
+  return $master_results;
+}
+
---
+++ enrol/wessync/helperlib.php
@@ -0,0 +1,232 @@
+<?php
+/*contains helper functions that didn't belong in the enroll object*/
+/*fetches ps89prod database object as thor*/
+function get_ps89prod_db() {
+  include "inc/db.conf.php";
+  $dbh = oci_connect($ps89prod_user,$ps89prod_pass,$ps89prod_dsn);
+  return $dbh;
+}
+
+/*fetches moodlecreate db object */
+function get_moodlecreate_db() {
+  include "inc/db.conf.php";
+  $moodlecreate = new mysqli($moodlecreate_host,$moodlecreate_user,$moodlecreate_pass,$moodlecreate_db,$moodlecreate_port);
+  return $moodlecreate;
+}
+
+/* takes a hash of course data and updates a given Moodle Course object with this data; use with caution! */
+function get_peoplesoft_course_data($psdbh,$course) {
+  $statement = "select * from sysadm.ps_wes_section where crse_id=:crseid and strm=:strm
+                and
+                class_nbr = wes_host_class_nbr";
+  $sth = oci_parse($psdbh,$statement);
+  oci_bind_by_name($sth,':strm',$course['term']);
+  oci_bind_by_name($sth,':crseid',$course['crse_id']);
+  oci_execute($sth);
+  $array_to_add = array('ACAD_CAREER','DESCRLONG');
+  while ($row = oci_fetch_array($sth,OCI_ASSOC)) {
+    foreach ($array_to_add as $field) {
+      $course[strtolower($field)] = $row[$field];
+    }
+  }
+  return $course;
+}
+
+
+/*checks for lock file, creates one if it does not exist - this *will* stick around if the script exits in an unpredictable fashion*/
+function check_lock_file($process_name) {
+  if (file_exists("/tmp/$process_name.lock")) {
+    echo "File already exists, script is running, please investigate!";
+    exit;
+  }
+  $file = fopen("/tmp/$process_name.lock",'w');
+  if ($file === false ) {
+    echo "Could not create lock file\n";
+    exit;
+  }
+  if (flock ($file, LOCK_EX | LOCK_NB ) === false ) {
+     echo "Script is running, please investigate!";
+     exit;
+  }
+  fwrite($file,getmypid());
+  return $file;
+}
+/*cleans up lock file*/
+function release_lock_file ($lock_file,$process) {
+   fclose($lock_file);
+   unlink("/tmp/$process.lock");
+}
+
+/* fetches moodle course based on idnumber, Wesleyan's unique identifier */
+function get_moodle_course ( $idnumber = '' ) {
+  global $DB;
+  $moodle_course = $DB->get_record('course',array('idnumber' => $idnumber));
+  if (!$moodle_course) {
+    return 0;
+   } else {
+    return $moodle_course;
+  }
+}
+
+/* Takes moodlecreate db handle and term and returns that have been requested */
+/* To Do: Clean up method that sections and instructors are fetched */
+function get_moodlecreate_courses( $db_handle = '', $term = '') {
+  if (!$db_handle) {
+    return false;
+  }
+  $moodlecreate_courses = array();
+  if ($term == '' ) {
+#    $stmt = $db_handle->prepare("SELECT a.TERM,a.CRSE_ID,a.SHORT_NAME,a.LONG_NAME from course a join o
+#n a.id == instructor.course_id and a.id == section.course_id  where a.created = 'Y' and a.term !='0'");
+#    $stmt = $db_handle->prepare("select c.TERM, c.CRSE_ID,s.section,c.SHORT_NAME,c.LONG_NAME, i.USERNAME
+#                                               from course c
+#                                               join instructor i on c.ID = i.COURSE_ID
+#                                               join section s on c.ID = s.course_ID
+#                                               order by c.CREATED_ON DESC, c.SHORT_NAME, i.USERNAME");
+     $stmt = $db_handle->prepare("select ID,TERM,CRSE_ID,SHORT_NAME,LONG_NAME,VISIBLE,REQUESTED_BY,STATUS from course");
+  } else {
+     $stmt = $db_handle->prepare("select ID,TERM,CRSE_ID,SHORT_NAME,LONG_NAME,VISIBLE,REQUESTED_BY,STATUS from course where TERM >= '$term'");
+   #TO BE IMPLEMENTED
+  }
+  $stmt->bind_result($id,$term,$crse_id,$short_name,$long_name,$visible,$requested_by,$status);
+  $stmt->execute();
+  while ($stmt->fetch()) {
+    $course['id'] = $id;
+    $course['short_name'] = addslashes($short_name);
+    $course['full_name'] = addslashes($long_name);
+    $course['term'] = $term;
+    $course['idnumber'] = $term . $crse_id;
+    $course['crse_id'] = $crse_id;
+    $course['instructor'] = array();
+    $course['section'] = array();
+    $course['status'] = $status;
+    if ($visible == 'Y' ) {
+      $course['visible'] = 1;
+    } else {
+      $course['visible'] = 0;
+    }
+    $course['requested_by'] = $requested_by;
+    $course['summary'] = '';
+    array_push($moodlecreate_courses,$course);
+  }
+ $stmt->close();
+
+  foreach ($moodlecreate_courses as &$sync_course) {
+    $stmt = $db_handle->prepare("SELECT SECTION FROM section where COURSE_ID=?");
+    $stmt->bind_param("i",$sync_course['id']);
+    $stmt->bind_result($section);
+    $stmt->execute();
+    while ($stmt->fetch()) {
+        $sync_course['idnumber'] .= $section;
+        array_push($sync_course['section'],$section);
+    }
+    $stmt = $db_handle->prepare("SELECT USERNAME FROM instructor where COURSE_ID=?");
+    $stmt->bind_param("i",$sync_course['id']);
+    $stmt->bind_result($instructor);
+    $stmt->execute();
+    while ($stmt->fetch()) {
+        array_push($sync_course['instructor'],$instructor);
+        $sync_course['summary'] .= "<p>Instructor: $instructor</p>";
+    }
+    $stmt->close();
+  }
+  return $moodlecreate_courses;
+}
+/*given an ldap_group return an array of usernames in that group */
+function get_from_ad ($ldap_group,$ldapconnection) { 
+ $sr = ldap_search($ldapconnection,"ou=AutoGroups,ou=WesUsers,dc=wesad,dc=wesleyan,dc=edu","(&(cn=$ldap_group))");
+  $entries = ldap_get_entries($ldapconnection,$sr);
+  $members = array();
+  if ($entries['count'] == 0) {
+    echo "No such group corresponding to $ldap_group\n";
+    return false;
+  } else {    $group = $entries[0];
+    for ($i = 0 ; $i < $group['member']['count'] ; $i++ ) {
+      $member = $group['member'][$i];
+      $sr = ldap_read($ldapconnection,$member,"(&(objectclass=user))",array('samaccountname'));
+      $entry = ldap_get_entries($ldapconnection,$sr);
+      array_push($members,$entry[0]['samaccountname'][0]);
+    }
+  }
+  if (sizeof($members) < 1 or !$members) {
+    return false;
+  }
+  return $members;
+}
+
+/*given a semester code finds all courses in a given year category */
+
+function wes_get_first_year_courses($semester) {
+  global $DB;
+  $fy_category =  $DB->get_record('course_categories',array('name' => 'First Year'));
+  $top_path = $fy_category->path;
+  $year = substr($semester,0,3)+ 1900; 
+  $sql = "path like '$top_path/%' and name like ?";
+  $category = $DB->get_record_select('course_categories',$sql,array($year),'id');
+  if (!isset($category->id)) {
+    return array();
+  } 
+  $courses = $DB->get_records('course',array('category' => $category->id));
+  return $courses; 
+}
+/*gets all "first year" students */
+   
+function wes_get_first_year_students($ps89prod,$semester) {   
+  $members = array();
+  /*from pturenne*/
+  $statement = "SELECT A.EMPLID
+  FROM SYSADM.PS_ACAD_PROG A, SYSADM.PS_ACAD_PLAN B
+  WHERE A.ACAD_CAREER = 'UGRD'
+     AND A.admit_term>=:strm
+     AND A.EMPLID = B.EMPLID
+     AND A.ACAD_CAREER = B.ACAD_CAREER
+     AND A.STDNT_CAR_NBR = B.STDNT_CAR_NBR
+     AND A.EFFSEQ = B.EFFSEQ
+     AND B.EFFDT = A.EFFDT
+     AND B.ACAD_PLAN in ('PRE-MATRIC','TCEX','VINT','FYST','TRAN') UNION SELECT C.EMPLID   FROM SYSADM.PS_SRVC_IND_DATA C   WHERE C.SRVC_IND_CD = 'NEW'      AND C.SRVC_IND_REASON IN ('ADVIS','INTER') AND C.AMOUNT = :year";
+  $year = substr($semester,0,3)+ 1900;
+  $sth = oci_parse($ps89prod,$statement);
+  oci_bind_by_name($sth,':strm',$semester);
+  oci_bind_by_name($sth,':year',$year);
+  if (!oci_execute($sth)) {
+    return false;
+  }
+  /*wesid->username */
+  $wesid_statement = "select sysadm.wes_get_email(:wesid) from dual";
+  $wesid_sth = oci_parse($ps89prod,$wesid_statement);
+  while ($row = oci_fetch_array($sth,OCI_ASSOC)) {
+    $emplid = $row['EMPLID'];
+    oci_bind_by_name($wesid_sth,':wesid',$emplid);
+    if (!oci_execute($wesid_sth)) {
+      return false;
+    }
+    $wesid_row = oci_fetch_row($wesid_sth);
+    $username = explode('@',$wesid_row[0]);
+    if ($username[1] == 'wesleyan.edu') {
+      array_push($members,$username[0]);
+    } else {
+      print "Email is $wesid_row[0] and is not Wesleyan for user id $emplid, skipping\n";
+    }
+  }
+  return array_unique($members);
+}
+/*updates moodlecreate database with updated status */
+function flag_as_created( $db_handle, $idnumber ) {
+   $stmt = $db_handle->prepare("update course set STATUS='Y' where ID=? and STATUS='N'");
+   $stmt->bind_param('d',$idnumber);
+   return $stmt->execute();
+}
+
+/*gets the season given a given semester */
+function get_season_from_semester($semester) {
+  $season = substr($semester,3,1);
+  if ($season == 9) {
+    $season = "Fall";
+  } else if ($season == 1 ) {
+    $season = "Spring";
+  } else if ($season == 6 ) {
+    $season = "Summer";
+  }
+  return $season;
+}
+
