9a4763f2c4615e336a1d6e97397b39cdd30eecbf
Matt Elson <melson@mugen.wesleyan.edu>
2012-07-05 11:17:21 -0400

    Wes - Added wessync enrolment plugin.

---
+++ enrol/wessync/lang/en/enrol_wessync.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'WesSync';
+$string['pluginname_desc'] = "Wesleyan's custom enrolment plugin with PeopleSoft, AD, etc..";
+$string['status'] = "WesSync Enable";
+$string['status_desc'] = "Enable or Disable Wes Peoplesoft Sync; you likely want this on :D";
