17346822714e8145ac5e8310f2404f97a2f58998
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2012-10-29 09:52:51 -0700

    adding coursewise javascript redirect to wes theme

--- theme/wesstandard/version.php
+++ theme/wesstandard/version.php
@@ -19,12 +19,12 @@
  *
  * @package    theme
  * @subpackage standard
- * @copyright  2011 Petr Skoda  {@link http://skodak.org}
+ * @copyright  2012 Wesleyan University
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2012091200; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2012102900; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2011081700; // Requires this Moodle version
 $plugin->component = 'theme_wesstandard'; // Full name of the plugin (used for diagnostics)
