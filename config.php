871a7f3d1be932dcc616917a35fcbf6be21ccc48
Daniel Landau <nprolog@postalmodern.com>
2013-07-10 15:19:57 -0700

    Squashed 'theme/cosmic/' content from commit 8405b6b
    git-subtree-dir: theme/cosmic
    git-subtree-split: 8405b6b258c080823287c784730e6bdc73e75952

---
+++ config.php
@@ -0,0 +1,162 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Theme configuration
+ *
+ * @package    theme_cosmic
+ * @copyright  2013 Lafayette College ITS
+ * @copyright  2012 Julian Ridden (original Rocket theme)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$THEME->name = 'cosmic';
+
+$THEME->parents = array('base');
+
+$THEME->sheets = array(
+    'layout',
+    'cosmic',
+    'menu',
+    'blocks',
+    'mod',
+    'calendar',
+    'dock',
+    'settings',
+    'rtl',
+    'tablet'
+);
+
+$THEME->parents_exclude_sheets = array(
+    'base'=>array(
+        'pagelayout',
+        'dock'
+    ),
+);
+$THEME->editor_sheets = array('editor');
+
+$THEME->layouts = array(
+    // Most backwards compatible layout without the blocks - this is the layout used by default.
+    'base' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+    ),
+    // Standard layout with blocks, this is recommended for most pages with default information.
+    'standard' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post', 'search'),
+        'defaultregion' => 'side-pre',
+    ),
+    // Main course page.
+    'course' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post', 'search'),
+        'defaultregion' => 'side-pre',
+        'options' => array('langmenu'=>true),
+    ),
+    'coursecategory' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post', 'search'),
+        'defaultregion' => 'side-pre',
+    ),
+    // Part of course, typical for modules - default page layout if $cm specified in require_login().
+    'incourse' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post', 'search'),
+        'defaultregion' => 'side-pre',
+    ),
+    // The site home page.
+    'frontpage' => array(
+        'file' => 'frontpage.php',
+        'regions' => array('side-pre', 'search', 'toplinks', 'homeblock'),
+        'defaultregion' => 'side-pre',
+        'options' => array('langmenu'=>true),
+    ),
+    // Server administration scripts.
+    'admin' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'search'),
+        'defaultregion' => 'side-pre',
+    ),
+    // My dashboard page.
+    'mydashboard' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'side-post', 'search'),
+        'defaultregion' => 'side-pre',
+        'options' => array('langmenu'=>true),
+    ),
+    // My public page.
+    'mypublic' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'search'),
+        'defaultregion' => 'side-pre',
+    ),
+    'login' => array(
+        'file' => 'login.php',
+        'regions' => array(),
+        'options' => array('langmenu'=>true),
+    ),
+
+    // Pages that appear in pop-up windows - no navigation, no blocks, no header.
+    'popup' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true, 'nonavbar'=>true, 'nocustommenu'=>true, 'nologininfo'=>true),
+    ),
+    // No blocks and minimal footer - used for legacy frame layouts only!
+    'frametop' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('nofooter'=>true),
+    ),
+    // Embedded pages, like iframe/object embeded in moodleform - it needs as much space as possible.
+    'embedded' => array(
+        'file' => 'embedded.php',
+        'regions' => array()
+    ),
+    // Used during upgrade and install, and for the 'This site is undergoing maintenance' message.
+    // This must not have any blocks, and it is good idea if it does not have links to
+    // other places - for example there should not be a home link in the footer...
+    'maintenance' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('noblocks'=>true, 'nofooter'=>true, 'nonavbar'=>true, 'nocustommenu'=>true),
+    ),
+    // Should display the content and basic headers only.
+    'print' => array(
+        'file' => 'general.php',
+        'regions' => array(),
+        'options' => array('noblocks'=>true, 'nofooter'=>true, 'nonavbar'=>false, 'nocustommenu'=>true),
+    ),
+    // The pagelayout used when a redirection is occuring.
+    'redirect' => array(
+        'file' => 'embedded.php',
+        'regions' => array()
+    ),
+    // The pagelayout used for reports.
+    'report' => array(
+        'file' => 'general.php',
+        'regions' => array('side-pre', 'search'),
+        'defaultregion' => 'side-pre',
+    ),
+);
+
+// Theme-specific settings; see base theme for additional options.
+$THEME->csspostprocess = 'cosmic_process_css';
+$THEME->enable_dock = true;
+$THEME->larrow = "<";
+$THEME->rarrow = ">";
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
---
+++ lang/en/theme_cosmic.php
@@ -0,0 +1,130 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_cosmic', language 'en'
+ *
+ * @package   theme_cosmic
+ * @copyright 2013 Lafayette College ITS
+ * @copyright 2012 Julian Ridden (original Rocket theme)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+$string['pluginname'] = 'Cosmic';
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['region-search'] = 'Search/Profile';
+$string['region-homeblock'] = 'Home Banner';
+$string['region-toplinks'] = 'Top Links';
+$string['choosereadme'] = '<div class="clearfix"><div class="theme_screenshot">
+
+<h1>Cosmic</h1>
+<img src="cosmic/pix/screenshot.png" />
+
+<h2>About</h2>
+<p>Cosmic is a content-fork of the Rocket theme. It was developed by Lafayette College ITS.</p>
+<p>Rocket is a three column fixed-width theme for Moodle 2.1+ originally designed by Julian Ridden to take Moodle themes to a more website focused look and feel. While the design is original, much of the CSS and the icons have come from <a href="http://moodle.org/user/profile.php?id=299683"><strong>Saun Daubney\'s</strong></a> fantastic Aardvark theme and also with support in the forums from <a href="http://moodle.org/user/profile.php?id=713800"><strong>Mary Evans</strong></a>.</p>
+
+<p>The theme is highly customisable through Moodle\'s theme settings. You can adjust images, text and colurs all without changing code. The theme also has a function that means that when users log in, courses they are enrolled in will show up in the dropdown \'custom menu\' at the top of the screen.</p>';
+
+$string['configtitle'] = 'cosmic theme';
+
+$string['homeblock'] = 'banner';
+
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'Change the logo of this theme by entering the URL to a new one (i.e., http://www.somesite/logo.png). As a reference, the default logo is 250px by 130px. A transparent .png will work best.';
+
+$string['footnote'] = 'Footnote';
+$string['footnotedesc'] = 'The content from this textarea will be displayed in the footer of every page. A great place for links.';
+
+$string['autohide'] = 'Autohide Functionality';
+$string['autohidedesc'] = 'Enabling this feature will change how moodle looks when editing mode is turned on. Edit icons only appear when hovering over a particular item or block. This makes the interface alot cleaner and less intimidating.';
+
+
+$string['editmodetoggle'] = 'Edit Mode Toggle';
+$string['edittoggledesc'] = 'When enabled this replaces the "Turn Editing On" button with a status toggle instead.';
+
+$string['enable'] = 'Enabled';
+$string['disable'] = 'Disabled';
+
+$string['mycoursetitle'] = 'Dropdown menu "My Course" Terminology';
+$string['mycoursetitledesc'] = 'Change the terminology for the "My Courses" link in the dropdown menu';
+$string['mycourses'] = 'My Courses';
+$string['myunits'] = 'My Units';
+$string['mymodules'] = 'My Modules';
+$string['myclasses'] = 'My Classes';
+$string['allcourses'] = 'All Courses';
+$string['allunits'] = 'All Units';
+$string['allmodules'] = 'All Modules';
+$string['allclasses'] = 'All Classes';
+
+$string['copyright'] = 'Copyright Notice';
+$string['copyrightdesc'] = 'This location is where you can put your copyright notice if you have one.';
+
+
+$string['sitename'] = 'Institution Name';
+$string['sitenamedesc'] = 'The name of your institution is shown on the frontpage. This is not the name of your site';
+
+$string['banner'] = 'Banner';
+$string['bannerdesc'] = 'Change the banner on the frontpage by entering the URL to a new one (i.e., http://www.somesite/banner.png). As a reference, the default banner is 750px by 250px.';
+
+$string['bannerheight'] = 'Homepage Banner Height';
+$string['nobanner'] = 'Disable Banner';
+$string['bannerheightdesc'] = 'If you are using the Banner location on the homepage you can change its defult height here. The max is 250px.';
+
+$string['themecolor'] = 'Theme Color';
+$string['themecolordesc'] = 'What color should your theme be. This will change mulitple components to produce the color you wish across the moodle site';
+
+$string['themetrimcolor'] = 'Theme Trim Color';
+$string['themetrimcolordesc'] = 'What color should your theme be. This will change mulitple components to produce the color you wish across the moodle site';
+
+$string['menucolor'] = 'Menu Color';
+$string['menucolordesc'] = 'Change the color of the menu';
+
+$string['contentlinkcolor'] = 'Content Link Color';
+$string['contentlinkcolordesc'] = 'Change the color of the links in content areas';
+
+$string['customcss'] = 'Custom CSS';
+$string['customcssdesc'] = 'For advanced users you can type in your own css here to override any of the theme defaults';
+
+$string['blocklinkcolor'] = 'Block Link Color';
+$string['blocklinkcolordesc'] = 'Change the color of the links in blocks';
+
+$string['menuhovercolor'] = 'Menu Hover Color';
+$string['menuhovercolordesc'] = 'Change the color of the menu items when the mouse hovers over them';
+
+$string['menutrimcolor'] = 'Menu Trim Color';
+$string['menutrimcolordesc'] = 'Change the color used to trim the edges of the menu';
+
+$string['menulinkcolor'] = 'Menu Link Color';
+$string['menulinkcolordesc'] = 'Change the color used for links in the menu';
+
+$string['navigation'] = 'navigation';
+
+$string['editingmode'] = '<strong>Editing Mode is now enabled:</strong> Please hover over the topic, item or block you wish to edit. Drag and drop files onto course sections to upload them';
+
+$string['screenwidth'] = 'Screen Width';
+$string['screenwidthdesc'] = 'This allows you to designate if this theme is displayed as a fixed with of 1000px or a variable width of 100%. <br> Please note that for design reasons the frontpage of the site will always remain fixed width';
+$string['fixedwidth'] = 'Fixed Width';
+$string['variablewidth'] = 'Variable Width';
+$string['turneditingoff'] = 'On';
+$string['turneditingon'] = 'Off';
+$string['editmode'] = 'Edit mode is:';
+$string['edittoggle'] = 'click to toggle';
+$string['breadcrumbwidth'] = 'Maximum breadcrumb width';
+$string['breadcrumbwidthdesc'] = 'The maximum permitted width for breadcrumbs in the navigation bar.';
