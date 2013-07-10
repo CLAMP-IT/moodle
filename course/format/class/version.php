df60eb8182e5afbac866bb798a542011e5a128c4
Sarah Ryder <sryder@hampshire.edu>
2013-07-10 16:33:18 -0400

    updated the custom class by date format version

--- course/format/class/version.php
+++ course/format/class/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2012042900;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2012042800.03;        // Requires this Moodle version.
+$plugin->version   = 2013071000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2012112900;        // Requires this Moodle version.
 $plugin->component = 'format_class';    // Full name of the plugin (used for diagnostics).
