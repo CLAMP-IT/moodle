36ce5b22b997a890db52dc1199caa7cbabbef840
Matt Elson <melson@wesleyan.edu>
2013-08-27 06:52:27 -0400

    WES - ME, added Mahara information and fixup to annotate plugin.

---
+++ mod/assignment/type/mahara/lang/en/assignment_mahara.php
@@ -0,0 +1,16 @@
+<?php
+$string['pluginname'] = 'Mahara portfolio';
+$string['clicktopreview'] = 'click to preview in full-size popup';
+$string['clicktoselect'] = 'click to select page';
+$string['nomaharahostsfound'] = 'No mahara hosts found.';
+$string['noviewscreated'] = 'You have not created any pages in {$a}.';
+$string['noviewsfound'] = 'No matching pages found in {$a}.';
+$string['preview'] = 'Preview';
+$string['site'] = 'Site';
+$string['site_help'] = 'This setting lets you select which Mahara site your students should submit their pages from. (The Mahara site must already be configured for mnet networking with this Moodle site.)';
+$string['selectedview'] = 'Submitted Page';
+$string['selectmaharaview'] = 'Select one of your {$a->name} portfolio pages from this complete list, or <a href="{$a->jumpurl}">click here</a> to visit {$a->name} and create a page right now.';
+$string['title'] = 'Title';
+$string['typemahara'] = 'Mahara portfolio';
+$string['views'] = 'Pages';
+$string['viewsby'] = 'Pages by {$a}';
