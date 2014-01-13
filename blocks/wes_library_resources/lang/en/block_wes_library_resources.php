c4c25b24c1976ee23fbc50833b35610569bcc0c0
Matthew Elson <melson@wesleyan.edu>
2014-01-13 16:06:11 -0500

    ME - Adding Wesleyan Library blocks.

---
+++ blocks/wes_library_resources/lang/en/block_wes_library_resources.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'Research Help';
+$string['simplehtml'] = 'Research Help';
+$string['simplehtml:addinstance'] = 'Add a new Research Help block';
+$string['simplehtml:myaddinstance'] = 'Add a new Research Help block to the My Moodle page';
+?>
