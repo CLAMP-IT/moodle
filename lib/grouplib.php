b7cb2f311cda4dc4da6bd84434bf4e8816005a2e
Sarah Ryder <sryder@hampshire.edu>
2014-08-19 15:39:56 -0400

    fixed assignment group bug as mentioned in MDL-46112

--- lib/grouplib.php
+++ lib/grouplib.php
@@ -866,6 +866,10 @@ function groups_filter_users_by_course_module_visible($cm, $users) {
     if (empty($cm->groupmembersonly)) {
         return $users;
     }
+    if (empty($users)) {
+        return $users;
+    }
+
     list($usql, $uparams) = $DB->get_in_or_equal(array_keys($users), SQL_PARAMS_NAMED, 'userid', true);
 
     // Group membership sub-query.
