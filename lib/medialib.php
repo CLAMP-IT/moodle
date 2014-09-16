c3870efbb1f0b86346275619cdea63ee24abecd4
Kevin Wiliarty <kevin.wiliarty@gmail.com>
2014-09-16 12:26:28 -0400

    prefers HTML5 over QuickTime player

--- lib/medialib.php
+++ lib/medialib.php
@@ -1084,7 +1084,7 @@ OET;
     }
 
     public function get_rank() {
-        return 20;
+        return 55;
     }
 }
 
@@ -1158,7 +1158,7 @@ OET;
     }
 
     public function get_rank() {
-        return 10;
+        return 54;
     }
 }
 
