2c17ebbac66a9c56280df4c24a4c68c113b9aa92
Matthew Elson <melson@wesleyan.edu>
2015-01-20 06:22:32 -0500

    ME - Library updates from Lori, approved.

--- blocks/wes_library_resources/block_wes_library_resources.php
+++ blocks/wes_library_resources/block_wes_library_resources.php
@@ -20,16 +20,20 @@ class block_wes_library_resources extends block_list
 1. How to get started with your research
 2. Make an appt with a librarian
 3. How to cite your sources
+4. Library FAQ
+5. Library how-to videos
 */
 	$this->content->items = array();
 	$this->content->icons = array();	
 	$this->content->items[] = '<a href="http://libraryfaq.wesleyan.edu/questions/3456" target="_blank">Plan and start your research</a>';
 	$this->content->items[] = '<a href="http://www.wesleyan.edu/libr/services/personalresearch.html" target="_blank">Meet with a librarian</a>';
 	$this->content->items[] = '<a href="http://libguides.wesleyan.edu/citing" target="_blank">How to cite your sources</a>';
+	$this->content->items[] = '<a href="http://libraryfaq.wesleyan.edu/" target="_blank">Library FAQ</a>';
+	$this->content->items[] = '<a href="http://www.wesleyan.edu/libr/find/showmehow.html" target="_blank">Library how-to videos</a>';
 	$this->content->icons[] = '';
 	$this->content->icons[] = '';
 	$this->content->icons[] = '';
-	$this->content->footer = 'FACULTY: For help using Moodle <a href="http://www.wesleyan.edu/its/services/teaching/acm.html">contact your ACM</a>';
+	$this->content->footer = '';
 
 		return $this->content;
 	} // get_content
