cdad0dc7c1590eed9fb320ed6c56bf8e5946f8b3
root <root@dumbledore.wesleyan.edu>
2015-04-29 09:24:48 -0400

    ME - WES Library Blocks.

---
+++ blocks/wes_library_chat/lang/en/block_wes_library_chat.php
@@ -0,0 +1,6 @@
+<?php
+$string['pluginname'] = 'Ask a Librarian';
+$string['simplehtml'] = 'Ask a Librarian';
+$string['simplehtml:addinstance'] = 'Add a new Ask a Librarian block';
+$string['simplehtml:myaddinstance'] = 'Add a new Ask a Librarian block to the My Moodle page';
+?>
---
+++ blocks/wes_library_resources/version.php
@@ -0,0 +1,4 @@
+<?php
+	defined('MOODLE_INTERNAL') || die();
+	$plugin->version = 2015011401;  
+	$plugin->requires = 2010112400; //  (This is the release version for Moodle 2.0)
