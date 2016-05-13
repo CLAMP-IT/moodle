986a1a4c26940833f378b8180b8196f04b32feeb
root <root@mcgonagall.wesleyan.edu>
2016-05-13 06:53:09 -0400

    Me - Latest 3.0 for 201617

--- blocks/wes_library_resources/en_block_wes_library_resources.php
+++
@@ -1,5 +0,0 @@
-<?php
-$string['pluginname'] = 'Research Help';
-$string['simplehtml'] = 'Research Help';
-$string['simplehtml:addinstance'] = 'Add a new Research Help block';
-$string['simplehtml:myaddinstance'] = 'Add a new Research Help block to the My Moodle page';
