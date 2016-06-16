e81a26fdd03e12407cab88f0fdf4340d39d8d50c
root <root@dumbledore.wesleyan.edu>
2016-06-16 09:42:14 -0400

    ME - Initial load of wes_library_main.

---
+++ blocks/wes_library_main/en_wes_block_library_main.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Library Research Help';
+$string['simplehtml'] = 'Library Research Help';
+$string['simplehtml:addinstance'] = 'Add a new Library Research Help block';
+$string['simplehtml:myaddinstance'] = 'Add a new Library Research Help block to the My Moodle page';
