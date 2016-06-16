c5f951c91d00da2fed85f46ebea010d4d4f932d5
root <root@dumbledore.wesleyan.edu>
2016-06-16 09:42:14 -0400

    ME - Initial load of wes_library_main.

---
+++ blocks/wes_library_main/version.php
@@ -0,0 +1,5 @@
+<?php
+	defined('MOODLE_INTERNAL') || die();
+	$plugin->component = 'block_wes_library_main';
+	$plugin->version = 2016061301;  
+	$plugin->requires = 2010112400; //  (This is the release version for Moodle 2.0)
