18df737409bee6a783a49bea5cb543d1a9f7779c
root <root@dumbledore.wesleyan.edu>
2016-12-09 10:20:10 -0500

    ME - Latest version of lightbox gallery.
    Conflicts:
    	mod/lightboxgallery/.travis.yml
    	mod/lightboxgallery/tests/behat/public_gallery.feature
    	mod/lightboxgallery/version.php

--- mod/lightboxgallery/view.php
+++ mod/lightboxgallery/view.php
@@ -56,6 +56,8 @@ require_login($course, true, $cm);
 
 if ($gallery->ispublic) {
     $userid = (isloggedin() ? $USER->id : 0);
+    $PAGE->set_cm($cm, $course);
+    $PAGE->set_pagelayout('incourse');
 } else {
     require_login($course, true, $cm);
     $userid = $USER->id;
@@ -98,7 +100,6 @@ if (has_capability('mod/lightboxgallery:edit', $context)) {
     $strediting = get_string('turnediting'.($editing ? 'off' : 'on'));
     $button = $OUTPUT->single_button($url, $strediting, 'get').' ';
 }
-$button .= $OUTPUT->update_module_button($cm->id, 'lightboxgallery');
 $PAGE->set_button($button);
 $PAGE->requires->css('/mod/lightboxgallery/assets/skins/sam/gallery-lightbox-skin.css');
 $PAGE->requires->yui_module('moodle-mod_lightboxgallery-lightbox', 'M.mod_lightboxgallery.init');
