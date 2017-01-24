c79015338c6c51622314807dba45302356de4180
root <root@dumbledore.wesleyan.edu>
2017-01-24 07:16:41 -0500

    ME - Annotate plugin update.

--- blocks/annotate/error.php
+++ blocks/annotate/error.php
@@ -1,15 +1,36 @@
-<?php 
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Annotate block error page to be displayed when problem transferring doc to Annotate
+ *
+ * @package   block_annotate
+ * @copyright Textensor Ltd.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require_once(dirname ( __FILE__ ) . '/../../config.php');
 ?>
 
 <html>
 <head>
 </head>
-<body style="padding : 60px">
-<h2>Error</h2>
-<p>
-There was a problem transferring to A.nnotate to view the document. Please contact
-your server administrator.
-</p>
+<body style="padding: 60px">
+    <h2><?php print get_string("transfer_error", "block_annotate"); ?></h2>
+    <p><?php print get_string("transfer_error_msg", "block_annotate"); ?></p>
+    <p><i><?php print optional_param('msg', "", PARAM_TEXT);; ?></i></p>
 </body>
-
 </html>
--- blocks/annotate/view.php
+++ blocks/annotate/view.php
@@ -1,58 +1,68 @@
 <?php
-require_once("../../config.php");
-require_once($CFG->dirroot.'/mod/resource/locallib.php');
-require_once($CFG->dirroot.'/course/lib.php');
-require_once($CFG->dirroot.'/course/format/lib.php');
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once('annotateUtil.php');
+/**
+ * Annotate block view.php - redirects to Annotate
+ *
+ * @package   block_annotate
+ * @copyright Textensor Ltd.
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
+require_once(dirname( __FILE__ ) . '/../../config.php');
+require_once($CFG->dirroot . '/mod/resource/locallib.php');
+require_once($CFG->dirroot . '/course/lib.php');
+require_once($CFG->dirroot . '/course/format/lib.php');
+require_once('lib.php');
 
-$id       = optional_param('id', 0, PARAM_INT); // resource ID
-
-$owner = optional_param('owner', "", PARAM_TEXT);  // if set, the doc should be owned by this person and
- 													 // just shared by the actual user
-
+$id    = optional_param('id', 0, PARAM_INT); // Resource ID.
+$owner = optional_param('owner', "", PARAM_TEXT); // If set, the doc should be owned by this person and just shared by the actual user.
 
 if (!$cm = get_coursemodule_from_id('resource', $id)) {
-        print_error('invalidcoursemodule');
+    print_error ('invalidcoursemodule');
 }
 
-$resource = $DB->get_record('resource', array('id'=>$cm->instance), '*', MUST_EXIST);
-
-
-$course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
+$resource = $DB->get_record('resource', array('id' => $cm->instance), '*', MUST_EXIST);
+$course = $DB->get_record('course', array('id' => $cm->course), '*', MUST_EXIST);
 
 require_course_login($course, true, $cm);
-$context = get_context_instance(CONTEXT_MODULE, $cm->id);
+$context = context_module::instance($cm->id);
+require_capability('block/annotate:accessannotate', $context);
 
 if ($resource->tobemigrated) {
     resource_print_tobemigrated($resource, $cm, $course);
-    die;
+    die();
 }
 
 $fs = get_file_storage();
 $files = $fs->get_area_files($context->id, 'mod_resource', 'content', 0, 'sortorder');
 if (count($files) < 1) {
     resource_print_filenotfound($resource, $cm, $course);
-    die;
+    die ();
 } else {
     $file = array_pop($files);
 }
 
-//$contextid, $component, $filearea, $itemid, $filepath, $filename
+$pathnamehash = $file->get_pathnamehash (); // Used for caching in annotate.
+if (! $file = $fs->get_file_by_hash ( $pathnamehash )) {
+    resource_print_filenotfound ( $resource, $cm, $course );
+    die ();
+}
+$docpath = '/' . $context->id . '/mod_resource/content/' . $resource->revision . $file->get_filepath () . $file->get_filename ();
 
-$pathnamehash = $file->get_pathnamehash();
-	if (!$file = $fs->get_file_by_hash($pathnamehash)) {
-   	 	resource_print_filenotfound($resource, $cm, $course);
-   		 die;
-	}
-    $docpath = '/'.$context->id.'/resources/'.$resource->revision.$file->get_filepath().$file->get_filename();
-    $code = uniqid();
-
-    $nameOfCourse = $course->fullname;
-    $courseId = $course->id;
-    exposeFileAndRedirect($docpath, $code, $pathnamehash, $owner , $nameOfCourse,$courseId);
-
-
-
-?>
+$coursename = $course->fullname;
+$courseid = $course->id;
+block_annotate_redirect_to_annotate($docpath, $pathnamehash, $owner, $coursename, $courseid);
