e8c68c6051dd0ef8d011ab5d12f83ee150284a1e
root <root@dumbledore.wesleyan.edu>
2017-01-25 09:59:24 -0500

    ME - adding panopto plugin.

---
+++ blocks/panopto/db/upgrade.php
@@ -0,0 +1,160 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Scripts used for upgrading database when upgrading block from an older version
+ *
+ * @package block_panopto
+ * @copyright  Panopto 2009 - 2016 with contributions from Spenser Jones (sjones@ambrose.edu)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Upgrades panopto for xmldb
+ *
+ * @param int $oldversion the previous version panopto is being upgraded from
+ */
+function xmldb_block_panopto_upgrade($oldversion = 0) {
+    global $CFG, $DB;
+    $dbman = $DB->get_manager();
+
+    if ($oldversion < 2014121502) {
+
+        // Add db fields for servername and application key per course.
+        if (isset($CFG->block_panopto_server_name)) {
+            $oldservername = $CFG->block_panopto_server_name;
+        }
+        if (isset($CFG->block_panopto_application_key)) {
+            $oldappkey = $CFG->block_panopto_application_key;
+        }
+
+        // Define field panopto_server to be added to block_panopto_foldermap.
+        $table = new xmldb_table('block_panopto_foldermap');
+        $field = new xmldb_field('panopto_server', XMLDB_TYPE_CHAR, '64', null, XMLDB_NOTNULL, null, null, 'panopto_id');
+
+        // Conditionally launch add field panopto_server.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+            if (isset($oldservername)) {
+                $DB->set_field('block_panopto_foldermap', 'panopto_server', $oldservername, null);
+            }
+        }
+
+        // Define field panopto_app_key to be added to block_panopto_foldermap.
+        $table = new xmldb_table('block_panopto_foldermap');
+        $field = new xmldb_field('panopto_app_key', XMLDB_TYPE_CHAR, '64', null, XMLDB_NOTNULL, null, null, 'panopto_server');
+
+        // Conditionally launch add field panopto_app_key.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+            if (isset($oldappkey)) {
+                $DB->set_field('block_panopto_foldermap', 'panopto_app_key', $oldappkey, null);
+            }
+        }
+
+        // Panopto savepoint reached.
+        upgrade_block_savepoint(true, 2014121502, 'panopto');
+    }
+
+    if ($oldversion < 2015012901) {
+
+        // Define field publisher_mapping to be added to block_panopto_foldermap.
+        $table = new xmldb_table('block_panopto_foldermap');
+        $field = new xmldb_field('publisher_mapping', XMLDB_TYPE_CHAR, '20', null, null, null, '1', 'panopto_app_key');
+
+        // Conditionally launch add field publisher_mapping.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Define field creator_mapping to be added to block_panopto_foldermap.
+        $table = new xmldb_table('block_panopto_foldermap');
+        $field = new xmldb_field('creator_mapping', XMLDB_TYPE_CHAR, '20', null, null, null, '3,4', 'publisher_mapping');
+
+        // Conditionally launch add field creator_mapping.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Panopto savepoint reached.
+        upgrade_block_savepoint(true, 2015012901, 'panopto');
+    }
+
+    if ($oldversion <= 2016101227) {
+        // Move block global settings to <prefix>_config_plugin table.
+        // First, move each server configuration. We are not relying here on
+        // block_panopto_server_number to determine number of servers, as there
+        // could be more. Moving all that we will find in order not to leave
+        // any abandoned config values in global configuration.
+        for ($x = 1; $x <= 10; $x++) {
+            if (isset($CFG->{'block_panopto_server_name' . $x})) {
+                set_config('server_name' . $x, $CFG->{'block_panopto_server_name' . $x}, 'block_panopto');
+                unset_config('block_panopto_server_name' . $x);
+            }
+            if (isset($CFG->{'block_panopto_application_key' . $x})) {
+                set_config('application_key' . $x, $CFG->{'block_panopto_application_key' . $x}, 'block_panopto');
+                unset_config('block_panopto_application_key' . $x);
+            }
+        }
+        // Now move block_panopto_server_number setting value.
+        if (isset($CFG->block_panopto_server_number)) {
+            set_config('server_number', $CFG->block_panopto_server_number, 'block_panopto');
+            unset_config('block_panopto_server_number');
+        }
+        // Move block_panopto_instance_name.
+        if (isset($CFG->block_panopto_instance_name)) {
+            set_config('instance_name', $CFG->block_panopto_instance_name, 'block_panopto');
+            unset_config('block_panopto_instance_name');
+        }
+        // Move block_panopto_async_tasks.
+        if (isset($CFG->block_panopto_async_tasks)) {
+            set_config('async_tasks', $CFG->block_panopto_async_tasks, 'block_panopto');
+            unset_config('block_panopto_async_tasks');
+        }
+        // Panopto savepoint reached.
+        upgrade_block_savepoint(true, 2016101227, 'panopto');
+    }
+
+    if ($oldversion < 2016102709) {
+        // Define table importmap where we will place all of our imports.
+        $table = new xmldb_table('block_panopto_importmap');
+
+        if (!$dbman->table_exists($table)) {
+            $importfields = array();
+            $importfields[] = new xmldb_field('id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL, true);
+            $importfields[] = new xmldb_field('target_moodle_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL);
+            $importfields[] = new xmldb_field('import_moodle_id', XMLDB_TYPE_INTEGER, '10', XMLDB_UNSIGNED, XMLDB_NOTNULL);
+
+            $importkey = new xmldb_key('primary', XMLDB_KEY_PRIMARY, array('id'), null, null);
+
+            foreach ($importfields as $importfield) {
+                // Conditionally launch add field import_moodle_id.
+                $table->addField($importfield);
+            }
+
+            $table->addKey($importkey);
+
+            $dbman->create_table($table);
+        }
+
+        // Panopto savepoint reached.
+        upgrade_block_savepoint(true, 2016102709, 'panopto');
+    }
+
+    return true;
+}
---
+++ blocks/panopto/lang/en/block_panopto.php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file contains all of the language strings needed by panopto.
+ *
+ * @package block_panopto
+ * @copyright  Panopto 2009 - 2016 with contributions from Spenser Jones (sjones@ambrose.edu)
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['add_to_panopto'] = 'Add this course to Panopto (re-add to sync user lists)';
+$string['back_to_config'] = 'Back to config';
+$string['back_to_course'] = 'Back to course';
+$string['block_edit_error'] = 'Cannot configure block instance: Global configuration incomplete. Please contact your system administrator.';
+$string['block_edit_header'] = 'Select the Panopto course to display in this block.';
+$string['block_edit_header_help'] = 'Choose an existing Panopto course or provision a new Panopto course for this block';
+$string['block_global_add_courses'] = 'Add Moodle courses to Panopto';
+$string['block_global_application_key'] = 'Application Key';
+$string['block_global_application_key_desc'] = 'Enter the Application Key from the Panopto Identity Providers page.';
+$string['block_panopto_publisher_system_role_mapping'] = 'System roles with provisioning permissions';
+$string['block_panopto_publisher_system_role_mapping_desc'] = 'Select which system roles can provision new Panopto courses.';
+$string['block_panopto_publisher_mapping'] = 'Publisher role mapping';
+$string['block_panopto_publisher_mapping_desc'] = 'Select which course roles are set as publishers in new Panopto courses.';
+$string['block_panopto_creator_mapping'] = 'Creator role mapping';
+$string['block_panopto_creator_mapping_desc'] = 'Select which course roles are set as creators in new Panopto courses.';
+$string['block_panopto_non_editing_teacher_provision'] = 'Allow non-editing teacher to provision';
+$string['block_panopto_non_editing_teacher_provision_desc'] = 'Enable this option to allow non-editing teachers to provision Panopto course folders.';
+$string['block_panopto_auto_sync_imports'] = 'Automatically grant permissions when importing a course';
+$string['block_panopto_auto_sync_imports_desc'] = 'Enable this option to allow Panopto to automatically grant viewer permissions when importing a course.';
+$string['block_global_hostname'] = 'Panopto Server Hostname';
+$string['block_global_hostname_desc'] = 'Enter the FQDN of your Panopto server.';
+$string['block_global_instance_desc'] = 'Enter the Instance Name from the Panopto Identity Providers page.';
+$string['block_global_instance_name'] = 'Moodle Instance Name';
+$string['block_panopto_server_number_desc'] = 'Click \'Save Changes\' to update number of servers.';
+$string['block_panopto_server_number_name'] = 'Number of Panopto Servers';
+$string['block_panopto_async_tasks'] = 'Asynchronous enrollment sync';
+$string['block_panopto_async_tasks_desc'] = 'Enable this option to allow course and enrollment tasks to run in the background.';
+$string['block_panopto_auto_provision'] = 'Automatically provision newly created courses';
+$string['block_panopto_auto_provision_desc'] = 'Enable this option to automatically provision a Panopto course folder when a course is created.';
+$string['completed_recordings'] = 'Completed Recordings';
+$string['course'] = 'Course';
+$string['course_name'] = 'Course Name';
+$string['course_settings'] = 'Course Settings';
+$string['creator'] = 'Creator';
+$string['creator_help'] = 'A Creator can create and edit content in Panopto';
+$string['creators'] = 'Creators';
+$string['download_recorder'] = 'Download Recorder';
+$string['error_retrieving'] = 'Error retrieving Panopto course.';
+$string['existing_course'] = 'Select an existing Panopto folder:';
+$string['fetching_content'] = 'Fetching Panopto Content...';
+$string['links'] = 'Links';
+$string['live_sessions'] = 'Live Sessions';
+$string['no_completed_recordings'] = 'No Completed Recordings';
+$string['no_course_selected'] = 'No Panopto course selected';
+$string['no_creators'] = 'No creators.';
+$string['no_live_sessions'] = 'No Live Sessions';
+$string['no_publishers'] = 'No publishers.';
+$string['no_server'] = 'There are no servers set up for provisioning. Please contact system administrator.';
+$string['no_students'] = 'No students.';
+$string['or'] = 'OR';
+$string['panopto:addinstance'] = 'Add a new Panopto block';
+$string['panopto:myaddinstance'] = 'Add a new Panopto block to my page';
+$string['panopto:provision_aspublisher'] = 'Provision as a publisher';
+$string['panopto:provision_asteacher'] = 'Provision as a teacher';
+$string['panopto:provision_course'] = 'Provision a course';
+$string['panopto:provision_multiple'] = 'Provision multiple courses at once';
+$string['pluginname'] = 'Panopto';
+$string['podcast_audio'] = 'Audio Podcast';
+$string['podcast_feeds'] = 'Podcast Feeds';
+$string['podcast_video'] = 'Video Podcast';
+$string['provision'] = 'Provision';
+$string['provision_course_link_text'] = 'Provision Course';
+$string['reprovision_course_link_text'] = 'Reprovision Course';
+$string['provision_courses'] = 'Provision Courses';
+$string['provision_error'] = 'Error provisioning course.';
+$string['provision_successful'] = 'Successfully provisioned course';
+$string['provisioncourseselect'] = 'Select Courses to Provision.';
+$string['provisioncourseselect_help'] = 'Multiple selections are possible by Ctrl-clicking (Windows) or Cmd-clicking (Mac).';
+$string['publisher'] = 'Publisher';
+$string['publisher_help'] = 'A Publisher can approve content submitted by Creators';
+$string['publishers'] = 'Publishers';
+$string['result'] = 'Result';
+$string['role_map_header'] = 'Change Panopto Role Mappings';
+$string['role_map_header_help'] = "Choose how Moodle roles map to Panopto roles. Unmapped Moodle roles will be assigned the Viewer role in Panopto";
+$string['select_server'] = 'Select a Panopto server';
+$string['select_server_help'] = 'Choose the Panopto server where the course folders will be provisioned.';
+$string['show_all'] = 'Show All';
+$string['show_less'] = 'Show Less';
+$string['students'] = 'Students';
+$string['take_notes'] = 'Take Notes';
+$string['unconfigured'] = 'Global configuration incomplete. Please contact your system administrator.';
+$string['unprovisioned'] = 'This course has not yet been provisioned.';
+$string['watch_live'] = 'Watch Live';
+/* End of file block_panopto.php */
